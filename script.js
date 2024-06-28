5d4f5f1ccd2294652b7131ad3a6077d98b792788
Mathilda Lee <junkyunglee@Mathildas-MacBook-Pro.local>
2024-06-28 11:05:04 +0900

    Add animations, pause button

--- script.js
+++ script.js
@@ -12,29 +12,75 @@ function timer_mode() {
     <div id="timer_input">
       <div class="container">
         <div class="label">Hours</div>
-        <input type="number" min="0" max="30" value="0" placeholder="Hour" />
+        <input id="timer_hr" type="number" min="0" max="30" value="0" placeholder="Hour" />
       </div>
       <div class="container">
         <div class="label">Minutes</div>
-        <input type="number" min="0" max="59" value="0" placeholder="Minute" />
+        <input id="timer_min" type="number" min="0" max="59" value="0" placeholder="Minute" />
       </div>
       <div class="container">
         <div class="label">Seconds</div>
-        <input type="number" min="0" max="59" value="0" placeholder="Second" />
+        <input id="timer_sec" type="number" min="0" max="59" value="0" placeholder="Second" />
       </div>
     </div>
     <div id="start_pause">
       <button id="timer_start" onclick="start_timer()">Start</button>
       <button id="timer_pause" onclick="pause_timer()">Pause</button>
+      <button id="timer_resume" onclick="resume_timer()">Resume</button>
     </div>
     <div id="timer">
     `;
 }
 
+let timerInterval;
+
 function start_timer() {
-  document.querySelector("#timer").innerHTML = `
-    <div>Hi</div>
-  `;
+  let hr = parseInt(document.querySelector("#timer_hr").value);
+  let min = parseInt(document.querySelector("#timer_min").value);
+  let sec = parseInt(document.querySelector("#timer_sec").value);
+
+  function updateDisplay() {
+    let displayHr = String(hr).padStart(2, "0");
+    let displayMin = String(min).padStart(2, "0");
+    let displaySec = String(sec).padStart(2, "0");
+
+    document.querySelector("#timer").innerHTML = `
+            <div id="timer_countdown">${displayHr} : ${displayMin} : ${displaySec}</div>
+        `;
+  }
+
+  function countdown() {
+    if (hr === 0 && min === 0 && sec === 0) {
+      clearInterval(timerInterval);
+      document.querySelector("#timer").innerHTML = `
+            <div class="done">Time's Up!</div>
+        `;
+      return;
+    }
+
+    if (sec > 0) {
+      sec--;
+    } else {
+      sec = 59;
+      if (min > 0) {
+        min--;
+      } else {
+        min = 59;
+        if (hr > 0) {
+          hr--;
+        }
+      }
+    }
+
+    updateDisplay();
+  }
+
+  updateDisplay();
+  timerInterval = setInterval(countdown, 1000);
+}
+
+function pause_timer() {
+  clearInterval(timerInterval);
 }
 
 function stopwatch_mode() {
